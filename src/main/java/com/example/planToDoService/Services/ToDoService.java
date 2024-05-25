834fd9ca2c0eee32dfc31320c45338d907f0bd5c
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-05-25 21:36:13 +0530

    done registration

--- src/main/java/com/example/planToDoService/Services/ToDoService.java
+++ src/main/java/com/example/planToDoService/Services/ToDoService.java
@@ -1,15 +1,29 @@
 package com.example.planToDoService.Services;
 
 import com.example.planToDoService.Models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ToDoService {
+    @Autowired
+    MongoTemplate mongoTemplate;
     public ResponseEntity<?> register(User user)
     {
-        return ResponseEntity.status(HttpStatus.OK).body("Hi");
+        String message;
+        if(user.getEmail()=="" || user.getPassword()=="")
+        {
+            message="Email or password is empty";
+        }
+        else
+        {
+            mongoTemplate.save(user);
+            message="user saved successfully";
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(message);
     }
 }
